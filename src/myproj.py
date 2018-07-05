29550109c6f5cc06145cb2c11e975887c194bbc0
Evan O'Connell <oconn.e@gmail.com>
2018-07-05 08:01:27 -0400

    Add PID to response

--- src/myproj.py
+++ src/myproj.py
@@ -1,9 +1,10 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style=\"color: blue\">Hello There!</h1>"
+    return "<h1 style=\"color: blue\">Hello There %d!</h1>" % (os.getpid())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
